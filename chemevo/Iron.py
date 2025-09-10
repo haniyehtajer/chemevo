b7ed082c37166f5f8124ea8888b5f929ad05b507
HaniyehTajer <tajer.1@osu.edu>
2025-09-10 12:18:21 -0400

    Fe completed

--- chemevo/Iron.py
+++ chemevo/Iron.py
@@ -2,9 +2,69 @@ import numpy as np
 
 from scipy.integrate import quad
 from scipy.integrate import cumulative_trapezoid as cumtrapz
+from scipy.integrate import simpson as simps
+
+def R_t(t_array, R_0, t_D, tau_Ia):
+    """
+    DTD of SNe Ia (eq. 1 in Weinberg et al. 2017)
+
+    Parameters
+    ----------
+    t_array : array_like
+        1D array of time values.
+    R_0 : float
+        Units of solar mass per year
+    t_D: foat
+        Minimum delay time for SN Ia (Gyr).
+    tau_Ia:
+        e-folding timescale of SN Ia DTD (Gyr).
+
+    Returns
+    -------
+    R_t : ndarray
+        SN Ia rate from population formed at t = 0 (Weinberg et al. 2017 eq. 1)
+    """
+    R_t = R_0 * np.exp(-(t_array - t_D)/tau_Ia)
+    R_t[np.where(t_array < t_D)] = 0
+    return R_t
+
+def Mdot_avg_Ia(t, t_array, Mdot_array, R_0, t_D, tau_Ia):
+    """
+    Compute <Mdot_*(t)>_Ia (eq. 6 Weinberg et al. 2017)
+
+    Parameters
+    ----------
+    t : float
+        Current time (Gyr)
+    t_array : ndarray
+        Grid of times (Gyr)
+    Mdot_array : ndarray
+        Star formation history, Mdot_*(t) (same length as t_array)
+    R_0, t_D, tau_Ia : floats
+        Parameters for the DTD
+
+    Returns
+    -------
+    Mdot_avg : float
+        <Mdot_*(t)>_Ia at time t
+    """
+    # Ensure integration only up to t
+    mask = t_array <= t
+    t_prime = t_array[mask]
+    Mdot_prime = Mdot_array[mask]
+
+    # Convolution term: Mdot(t') * R(t - t')
+    R_vals = R_t(t - t_prime, R_0, t_D, tau_Ia)
+    numerator = simps(Mdot_prime * R_vals, t_prime)
+
+    # Normalization: ∫ R(t') dt'
+    t_grid = np.linspace(0, 20, 5000)  # large enough to approximate ∞
+    denominator = simps(R_t(t_grid, R_0, t_D, tau_Ia), t_grid)
+
+    return numerator / denominator
 
 
-def solve_for_Z_Fe(t_array, M_g_array, tau_dep, o_yield, tau_star):
+def solve_for_Z_Fe(t_array, M_g_array, tau_dep, m_Fe_Ia, m_Fe_cc, tau_star, R_0, t_D, tau_Ia):
     """
     Solve for the Iron abundance Z_O(t), given gas mass M_g(t) as a function of time.
 
@@ -16,26 +76,39 @@ def solve_for_Z_Fe(t_array, M_g_array, tau_dep, o_yield, tau_star):
         1D array of gas mass values corresponding to `t_array`.
     tau_dep : float
         Gas depletion timescale (Gyr).
-    o_yield : float
+    m_Fe_Ia : float
+        IMF-integrated SN Ia Iron yield.
+    m_Fe_cc : float
         IMF-integrated CCSN Iron yield.
     tau_star : float
         Star formation efficiency (SFE) timescale (Gyr).
+    R_0 : float
+        Units of solar mass per year
+    t_D: foat
+        Minimum delay time for SN Ia (Gyr).
+    tau_Ia:
+        e-folding timescale of SN Ia DTD (Gyr).
 
     Returns
     -------
-    Z_O_array : ndarray
+    Z_Fe_array : ndarray
         Iron abundance as a function of time, evaluated on `t_array`.
     
     """
+    Mdot = M_g_array/tau_star
+    M_dot_averages = np.zeros(len(t_array))
+    for i in range(len(t_array)):
+        M_dot_averages[i] = Mdot_avg_Ia(t_array[i], t_array, Mdot, R_0, t_D, tau_Ia)
 
     p_t = 1/tau_dep * np.ones(len(t_array))
-    f_t = o_yield / tau_star * M_g_array
+    f_t = m_Fe_cc / tau_star * M_g_array + m_Fe_Ia * M_dot_averages
+
     integral_p = cumtrapz(p_t, t_array, initial=0)
     mu_t = np.exp(integral_p)
     integral_mu_f = cumtrapz(mu_t * f_t, t_array, initial=0)
-    M_O_array = 1/mu_t * integral_mu_f
-    Z_O_array = M_O_array/M_g_array
-    return Z_O_array
+    M_Fe_array = 1/mu_t * integral_mu_f
+    Z_Fe_array = M_Fe_array/M_g_array
+    return Z_Fe_array
 
 
 
