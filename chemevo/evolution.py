b7750cc1e416871b6b0a78812acfdb8c936df0d5
HaniyehTajer <tajer.1@osu.edu>
2025-09-14 15:21:43 -0500

    eulerian method is done, need to add eqs and analytics to obbj oriented theme

--- chemevo/evolution.py
+++ chemevo/evolution.py
@@ -1,10 +1,11 @@
 import numpy as np
+from scipy.integrate import simpson as simps
 
 class Galaxy:
     def __init__(self, t_array,
                  m_o_cc=0.015, eta=2.5, r=0.4, tau_star=1.0,
                  m_Fe_cc=0.0012, m_Fe_Ia=0.0017,
-                 tau_sfh=6.0, tau_Ia=1.5, t_D=0.15):
+                 tau_sfh=6.0, tau_Ia=1.5, t_D=0.15, DTD_R0 = 2.2 * 10**(-3)):
         """
         Initialize the chemical evolution model.
 
@@ -19,6 +20,7 @@ class Galaxy:
         self.t = t_array
         self.dt = t_array[1] - t_array[0]
         self.n_steps = len(t_array)
+        self.DTD_R0 = DTD_R0
 
         # Core parameters
         self.m_o_cc_arr   = self._make_array(m_o_cc)
@@ -31,12 +33,14 @@ class Galaxy:
         self.m_Fe_Ia_arr  = self._make_array(m_Fe_Ia)
         self.tau_sfh_arr  = self._make_array(tau_sfh)
         self.tau_Ia_arr   = self._make_array(tau_Ia)
-        self.t_D_arr      = self._make_array(t_D)
+        self.t_D = t_D
 
         # Placeholders for results
         self._m_O = None
         self._z_O = None
-        self._tau_dep = None
+        self._m_Fe = None
+        self._z_Fe = None
+        self.tau_dep_arr = self.compute_tau_dep()
 
     def _make_array(self, param):
         """Convert a scalar into a constant array matching self.t."""
@@ -72,13 +76,12 @@ class Galaxy:
         m_O : np.ndarray
         """
         m_O = np.zeros(self.n_steps)
-        tau_dep = self.compute_tau_dep()
         for i in range(1, self.n_steps):
             m_O[i] = (
                 m_O[i-1]
                 + self.dt * (
                     (self.m_o_cc_arr[i-1] * m_g_array[i-1] / self.tau_star_arr[i-1])
-                    - (m_O[i-1]/tau_dep[i-1])
+                    - (m_O[i-1]/self.tau_dep_arr[i-1])
                 )
             )
         self._m_O = m_O
@@ -92,6 +95,43 @@ class Galaxy:
         self._z_O = z_O
         return z_O
     
+    def DTD_exp(self):
+        """Compute exponential DTD."""
+        DTD_exp_array = self.DTD_R0 * np.exp(-(self.t - self.t_D)/self.tau_Ia_arr)
+        DTD_exp_array[np.where(self.t < self.t_D)] = 0
+        return DTD_exp_array
+        
+    def compute_mdotstar_Ia(self, m_g_array, r_t_array=None):
+        if r_t_array is None:
+            r_t_array = self.DTD_exp() 
+        mdotstar_Ia = np.zeros(self.n_steps)
+        r_t_inf = np.sum(r_t_array * self.dt)
+        mdotstar = m_g_array/self.tau_star_arr
+        for i in range(1, self.n_steps):
+            for j in range(i):
+                mdotstar_Ia[i] += (mdotstar[j] * r_t_array[i - j] * self.dt)/r_t_inf
+        return mdotstar_Ia
     
+    def integrate_m_Fe(self, m_g_array):
+        m_Fe = np.zeros(self.n_steps)
+        mdotstar_Ia = self.compute_mdotstar_Ia(m_g_array=m_g_array)
+        for i in range(1, self.n_steps):
+            m_Fe[i] = m_Fe[i-1] + self.dt*( (self.m_Fe_cc_arr[i-1] * m_g_array[i-1] / self.tau_star_arr[i-1])
+                                           + (self.m_Fe_Ia_arr[i-1] * mdotstar_Ia[i-1])
+                                            - m_Fe[i-1]/self.tau_dep_arr[i-1] )
+        self._m_Fe = m_Fe
+        return m_Fe
+    
+    def compute_z_Fe(self, m_g_array):
+        if self._m_Fe is None:
+            self.integrate_m_Fe(m_g_array)
+        z_Fe = self._m_Fe / m_g_array
+        self._z_Fe = z_Fe
+        return z_Fe
+
+
+
+
+
     
 
